83e9fad38f811f61b7c8443073ee354cb75df2c6
Vanja Vasic <vanja.vasic@yahoo.com>
2024-03-19 18:45:26 +0100

    rename modal selector

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
+  selector: 'modal',
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
