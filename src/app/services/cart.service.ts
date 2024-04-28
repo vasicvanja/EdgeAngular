b32f34ab3787365c4c7f6a71a2766a7a895191f1
Vanja Vasic <vanja.vasic@yahoo.com>
2024-04-28 19:58:28 +0200

    clear the cart items after a successful payment and show the bought items

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,6 +12,7 @@ import { environment } from '../../environments/environment';
 })
 export class CartService {
     private cartItems = new BehaviorSubject<Artwork[]>([]);
+    private lastPurchasedItems = new BehaviorSubject<Artwork[]>([]);
     private baseUrl: string;
 
     constructor(private http: HttpClient, private toastrService: ToastrService) {
@@ -19,27 +20,49 @@ export class CartService {
         this.loadCartItems();
     }
 
+    // This method returns an Observable of the current cart items.	
+    // An Observable is a stream of values that can be subscribed to.
     getCartItems$() {
         return this.cartItems.asObservable();
     }
 
+    // This method returns an Observable of the total price of the items in the cart. It uses the map operator 	
+    // to transform the Observable of cart items into an Observable of the total price. The reduce function 	
+    // is used to calculate the total price by summing up the price of each item.
     getTotalPrice$() {
         return this.cartItems.asObservable().pipe(
             map(items => items.reduce((total, item) => total + item.Price * item.Quantity, 0))
         );
     }
 
+    // The getCartItemsCount$() method returns an Observable of the total number of items in the cart.	
+    // It uses the map operator to transform the Observable of cart items into an Observable of the 	
+    // count of items. The length property of the array is used to calculate the total number of items. 	
+    // This method can be subscribed to for getting updates whenever the number of items in the cart changes.	
+    // This can be particularly useful for updating UI elements that display the number of items in the cart
     getCartItemsCount$() {
         return this.cartItems.asObservable().pipe(
             map(items => items.reduce((count, item) => count + item.Quantity, 0))
         );
     }
 
+    // This method returns an Observable of the last purchased items.
+    getLastPurchasedItems$() {
+        return this.lastPurchasedItems.asObservable();
+    }
+
+    // This method clears the cart and saves the current cart items as the last purchased items
+    // if the stripe payment is successful.
     clearCart() {
+        this.lastPurchasedItems.next([...this.cartItems.value]);
         this.cartItems.next([]);
         this.saveCartItems();
     }
 
+    // This method adds an artwork to the cart. It first gets the current cart items, 	
+    // then pushes the new artwork to this array, and finally updates the cartItems 	
+    // BehaviorSubject with the new array. After updating the cart items, it calls 	
+    // saveCartItems() to persist the new cart items in LocalStorage.
     addToCart(artwork: Artwork) {
         const currentItems = this.cartItems.value;
         const foundItem = currentItems.find(item => item.Id === artwork.Id);
@@ -62,6 +85,10 @@ export class CartService {
         }
     }
 
+    // This method removes an artwork from the cart. It first gets the current cart items,
+    // then finds the index of the artwork to be removed, and if the artwork is found in 
+    // the cart, it removes it from the array. Finally, it updates the cartItems BehaviorSubject
+    // with the new array and calls saveCartItems() to persist the new cart items in LocalStorage.
     removeFromCart(artwork: Artwork) {
         const currentItems = this.cartItems.value;
         const itemToRemove = currentItems.find(item => item.Id === artwork.Id);
@@ -80,6 +107,9 @@ export class CartService {
         }
     }
 
+    // This private method saves the current cart items in LocalStorage along with 
+    // the current timestamp. It usesJSON.stringify to convert the array of cart 
+    // items into a JSON string, because LocalStorage can only store strings.
     saveCartItems() {
         const cartData = {
             items: this.cartItems.value,
@@ -88,6 +118,11 @@ export class CartService {
         localStorage.setItem('cartItems', JSON.stringify(cartData));
     }
 
+    // This private method loads the cart items from LocalStorage. It uses JSON.parse
+    // to convert the JSON string back into an array of cart items. If there are 
+    // saved cart items in LocalStorage, it updates the cartItems BehaviorSubject with these items.
+    // The method checks the timestamp when loading the cart items. If the data is older than the 
+    // specified lifetime, it removes the data from LocalStorage.
     loadCartItems() {
         const cartItems = localStorage.getItem('cartItems');
         if (cartItems) {
@@ -106,6 +141,8 @@ export class CartService {
         }
     }
 
+    // This method sends a POST request to the server to create a Stripe checkout session
+    // the given artworks. It returns a Promise that resolves to the response from the server.
     public createCheckoutSession = (artworks: Artwork[]): any => {
         return firstValueFrom(this.http.post(this.baseUrl + "/api/Stripe/createCheckoutSession", artworks));
     }
