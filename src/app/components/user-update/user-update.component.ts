a6ce8f3b87847326c34cce4891f8ff3c92583e86
Vanja Vasic <vanja.vasic@yahoo.com>
2025-03-25 00:29:16 +0100

    create user update component

---
+++ src/app/components/user-update/user-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-update',
+  imports: [],
+  templateUrl: './user-update.component.html',
+  styleUrl: './user-update.component.scss'
+})
+export class UserUpdateComponent {
+
+}
